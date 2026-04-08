8485b069cc6e2ae0543b0368a420200ae0275780
Joaquin <joaquin.leon@xidden.tech>
2026-04-08 09:55:53 -0600

    Add path alias for src directory in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "node:path";
 
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss(), tsconfigPaths()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
   },
   image: {
     domains: ["darkgrey-alpaca-160443.hostingersite.com", "coffee.local"],
